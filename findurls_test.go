2a1a8efd6b05c3a2d50036a2875561789e6b155b
Juliane Clausen <git@juliane-clausen.de>
2014-08-13 22:30:25 +0200

    reformatted

--- findurls_test.go
+++ findurls_test.go
@@ -1,20 +1,21 @@
 package main
 
-import(
-        "testing"
+import (
+	"testing"
 )
 
 func TestFindURLs(t *testing.T) {
-        var res []string
-        var teststrings = map[string][]string{
-                "Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=bar sagt?":[]string{"http://starship-factory.ch/blah?foo=bar"},
-                "Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=bar http://foo?baz=quux sagt?":[]string{"http://starship-factory.ch/blah?foo=bar", "http://foo?baz=quux"},
-                "http://starship-factory.ch/blah?foo=bar.":[]string{"http://starship-factory.ch/blah?foo=bar"},
-        }
-        for teststring, expected := range teststrings {
-                res = FindURLs(teststring)
-                if len(res) != len(expected) {
-                        t.Error("Wrong number of URLs. Expected: ", expected, ", got ", res, ".")
-                }
-        }
+	var res []string
+	var teststrings = map[string][]string{
+		"Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=bar sagt?":                     []string{"http://starship-factory.ch/blah?foo=bar"},
+		"Wie sieht es aus wenn man https://starship-factory.ch/blah?foo=bar sagt?":                    []string{"http://starship-factory.ch/blah?foo=bar"},
+		"Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=bar http://foo?baz=quux sagt?": []string{"http://starship-factory.ch/blah?foo=bar", "http://foo?baz=quux"},
+		"http://starship-factory.ch/blah?foo=bar.":                                                    []string{"http://starship-factory.ch/blah?foo=bar"},
+	}
+	for teststring, expected := range teststrings {
+		res = FindURLs(teststring)
+		if len(res) != len(expected) {
+			t.Error("Wrong number of URLs. Expected: ", expected, ", got ", res, ".")
+		}
+	}
 }
