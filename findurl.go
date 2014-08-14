a331b03b25f101026d3e0396688b7b553cbf4c5a
Juliane Clausen <git@juliane-clausen.de>
2014-08-14 23:46:20 +0200

    Repaired regexp, added more tests.

--- findurl.go
+++ findurl.go
@@ -5,8 +5,14 @@ import (
 )
 
 // Global regexp vars
-var url_re = regexp.MustCompile(`\bhttps?://[^\s]+\b`) // Finds URLs
+var url_re = regexp.MustCompile(`\b(https?://\S+)[\s\.$]`) // Finds URLs
 
-func FindURLs(input string) []string {
-	return url_re.FindAllString(input, 5)
+func FindURLs(input string) (ret []string) {
+        var matches = url_re.FindAllStringSubmatch(input, 5)
+        for _, value := range matches {
+                if len(value) >= 2 {
+                        ret = append(ret, value[1])
+                }
+        }
+        return
 }
--- findurls_test.go
+++ findurls_test.go
@@ -10,17 +10,19 @@ func TestFindURLs(t *testing.T) {
 		"Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=1bar sagt?":                     []string{"http://starship-factory.ch/blah?foo=1bar"},
 		"Wie sieht es aus wenn man https://starship-factory.ch/blah?foo=2bar sagt?":                    []string{"https://starship-factory.ch/blah?foo=2bar"},
 		"Wie sieht es aus wenn man http://starship-factory.ch/blah?foo=3bar http://foo?baz=quux sagt?": []string{"http://starship-factory.ch/blah?foo=3bar", "http://foo?baz=quux"},
-		"http://starship-factory.ch/blah?foo=bar.":                                                    []string{"http://starship-factory.ch/blah?foo=bar"},
+		"http://starship-factory.ch/blah?foo=bar.":                                                     []string{"http://starship-factory.ch/blah?foo=bar"},
+		"Dieser freche Text enthaelt einfach keine URLs!":                                              []string{},
+                "http://eins/ http://zwei/ http://drei/ http://vier/ http://fuenf/ http://sechs/ http://sie/":  []string{"http://eins/", "http://zwei/", "http://drei/", "http://vier/", "http://fuenf/"},
 	}
 	for teststring, expected := range teststrings {
 		res = FindURLs(teststring)
 		if len(res) != len(expected) {
 			t.Error("Wrong number of URLs. Expected: ", expected, ", got ", res, ".")
 		}
-                for i := range res {
-                        if res[i] != expected[i] {
-                                t.Error("Wrong URL. Expected: ", expected[i], " got ", res[i], "." )
-                        }
-                }
+		for i := range res {
+			if res[i] != expected[i] {
+				t.Error("Wrong URL. Expected: ", expected[i], " got ", res[i], ".")
+			}
+		}
 	}
 }
